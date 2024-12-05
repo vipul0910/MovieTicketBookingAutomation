9e58ba46847ebcf3a680f5cd0f182259885edab8
vipul <vipulsingh199811@gmail.com>
2024-12-05 14:27:18 +0530

    Main files

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+# config/settings.py
+BASE_URL = "https://in.bookmyshow.com/"
+CITY = "Chennai"
+MOVIE_NAME = "Moana 2"
+SEAT_COUNT = 2
+BROWSER_DRIVER_PATH = "./chromedriver/chromedriver"
+LOG_FILE_PATH = "./logs/booking.log"
